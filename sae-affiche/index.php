657c138be978b925cdd5f3e0a3f46616ac773731
Vor413 <vivi.finlande@gmail.com>
2025-05-05 18:00:38 +0200

    Add files via upload

---
+++ sae-affiche/index.php
@@ -0,0 +1,27 @@
+<?php
+	require 'helpers/functions.php';
+	echo html_head("Concours d'affiches");
+?>
+<body>
+	<header class="container">
+		<h1>Voici la liste des concours à venir</h1>
+	</header>
+	<main class="container">
+		<div class="concours">
+			<h2>The truman show</h2>
+			<p>Date ouverture : 10 mars 2025 - Date de cloture : 25 mars 2025</p>
+			<hr />
+		</div>
+		<div class="concours">
+			<h2>Sommet des dieux</h2>
+			<p>Date ouverture : 15 mars 2025 - Date de cloture : 30 mars 2025</p>
+			<hr />
+		</div>
+		<div class="concours">
+			<h2>Lady Bird</h2>
+			<p>Date ouverture : 25 mars 2025 - Date de cloture : 10 avril 2025</p>
+			<hr />
+		</div>
+	</main>
+</body>
+</html>
